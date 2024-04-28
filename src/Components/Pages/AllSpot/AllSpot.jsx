16d7ea8dc8b887996c5238e2b39f65c3c966fbb2
farha12345678 <sabikunnaharfarha01@gmail.com>
2024-04-29 04:14:18 +0600

    8th commit

--- src/Components/Pages/AllSpot/AllSpot.jsx
+++ src/Components/Pages/AllSpot/AllSpot.jsx
@@ -5,59 +5,64 @@ import AllSpotCard from "./AllSpotCard";
 
 const AllSpot = () => {
     const spotLoad = useLoaderData()
+    
 
-    const [displaySpot, setDisplaySpot] = useState(spotLoad)
-
-
-    const handleFilter = filter => {
-
-        if (filter === 'All') {
-            setDisplaySpot(spotLoad)
-        } else if (filter === 'Bangladesh') {
-            const spotBd = spotLoad.filter(spot => spot.country === 'Bangladesh')
-            setDisplaySpot(spotBd)
-        }
-        else if (filter === 'Thailand') {
-            const spotBd = spotLoad.filter(spot => spot.country === 'Thailand')
-            setDisplaySpot(spotBd)
-        }
-        else if (filter === 'Malaysia') {
-            const spotBd = spotLoad.filter(spot => spot.country === 'Malaysia')
-            setDisplaySpot(spotBd)
-        }
-        else if (filter === 'Vietnam') {
-            const spotBd = spotLoad.filter(spot => spot.country === 'Vietnam')
-            setDisplaySpot(spotBd)
-        }else if (filter === 'Cambodia') {
-            const spotBd = spotLoad.filter(spot => spot.country === 'Cambodia')
-            setDisplaySpot(spotBd)
-        }
-        else if (filter === 'Indonesia') {
-            const spotBd = spotLoad.filter(spot => spot.country === 'Indonesia')
-            setDisplaySpot(spotBd)
+    const [sort, setSort] = useState();
+    const parseCost = (cost) => {
+        if(cost === "Free" || cost === "free"){
+            return 0;
+        }else{
+return parseFloat(cost.replace(/[^0-9.-]+/g,""))
         }
         
     }
+
+    const handleSort = () => {
+        if(sort === "asc"){
+            sort.sort(
+                (a,b)=>parseCost(a.average_cost) - parseCost(b.average_cost)
+            );
+            setSort("desc")
+        }else{
+            sort.sort(
+                (a,b)=>parseCost(b.average_cost) - parseCost(a.average_cost)
+            );setSort("asc")
+        }
+    }
+    const handleSortChange = (e) => {
+        setSort(e.target.value);
+    };
+    // const handleSort = () => {
+    //     const sortedSpotLoad = [...spotLoad].sort((a, b) => {
+    //         const costA = parseCost(a.average_cost);
+    //         const costB = parseCost(b.average_cost);
+    //         return sort === 'asc' ? costA - costB : costB - costA;
+    //     });
+    //     // setSort(sort === 'asc' ? 'desc' : 'asc');
+       
+    //     setSort(sortedSpotLoad);
+    // };
+
+    
+
+
+    
     return (
         <div>
             <div className="text-center my-10">
                 <h1 className="font-semibold text-4xl">All Tourists Spots</h1>
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn m-1">Select Country</div>
-                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                        <li onClick={()=>handleFilter('All')}><a>All</a></li>
-                        <li onClick={()=>handleFilter('Bangladesh')}><a>Bangladesh</a></li>
-                        <li onClick={()=>handleFilter('Thailand')}><a>Thailand</a></li>
-                        <li onClick={()=>handleFilter('Vietnam')}><a>Vietnam</a></li>
-                        <li onClick={()=>handleFilter('Indonesia')}><a>Indonesia</a></li>
-                        <li onClick={()=>handleFilter('Cambodia')}><a>Cambodia</a></li>
-                    </ul>
-                </div>
+                <div className="text-center">
+                <select value={setSort} onChange={handleSortChange}>
+                    <option value="asc">Ascending</option>
+                    <option value="desc">Descending</option>
+                </select>
+                <button onClick={handleSort}>Sort</button>
+            </div>
 
             </div>
             <div className="grid lg:grid-cols-2 grid-cols-1 gap-y-4 mx-5">
                 {
-                    displaySpot.map(spots => <AllSpotCard key={spots._id} spots={spots}></AllSpotCard>)
+                    spotLoad.map(spots => <AllSpotCard key={spots._id} spots={spots}></AllSpotCard>)
                 }
             </div>
 
